5a6fcb668beb51b386de84ac0153d4a1708b41da
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-07 14:21:49 -0500

    Refine concurrent computation method

--- golang/main.go
+++ golang/main.go
@@ -8,44 +8,60 @@ import (
 
 func main() {
 	numberList := generateNumList(100000000)
-	middle := len(numberList) / 2
-	firstHalf := numberList[:middle]
-	secondHalf := numberList[middle:]
+	numberOfGroups := 5
+	splitList := splitList(numberList, numberOfGroups)
 
+	ch := make(chan int64, numberOfGroups)
 	var wg sync.WaitGroup
-	var sum1, sum2 int
 
-	wg.Add(2)
-	go func() {
-		defer wg.Done()
-		sum1 = sumList(firstHalf)
-	}()
-	go func() {
-		defer wg.Done()
-		sum2 = sumList(secondHalf)
-	}()
+	for _, list := range splitList {
+		wg.Add(1)
+		go beginSumming(list, ch, &wg)
+	}
 	wg.Wait()
 
-	total := sum1 + sum2
-	fmt.Println("Total:", total)
+	var totalSum int64
+	close(ch)
+	for num := range ch {
+		totalSum += num
+	}
+
+	fmt.Println("Total sum:", totalSum)
 }
 
-func sumList(numbers []int) int {
-	total := 0
+func beginSumming(nums []int64, ch chan int64, wg *sync.WaitGroup) {
+	defer wg.Done()
+	result := sumList(nums)
+	ch <- result
+}
+
+func splitList(list []int64, pieces int) [][]int64 {
+	subListLength := (len(list)) / pieces
+	resultList := make([][]int64, pieces)
+	for i := 0; i < pieces; i++ {
+		start := i * subListLength
+		end := start + subListLength
+		resultList[i] = list[start:end]
+	}
+	return resultList
+}
+
+func sumList(numbers []int64) int64 {
+	var total int64
 	for _, num := range numbers {
 		total += num
 	}
-	return total 
+	return total
 }
 
-func generateNumList(length int) []int {
-	nums := make([]int, length)
+func generateNumList(length int) []int64 {
+	nums := make([]int64, length)
 	for i := 0; i < length; i++ {
-		nums[i] = randomNumberUnder(100)
+		nums[i] = randomNumUnder(100)
 	}
 	return nums
 }
 
-func randomNumberUnder(num int) int {
-	return rand.Int() % num
+func randomNumUnder(num int) int64 {
+	return int64(rand.Int() % num)
 }
